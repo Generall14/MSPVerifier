7a74775203c7e17d2267fc46a1a58df1e1cdd39c
Wojciech Kogut <wojciech_kogut@o2.pl>
2018-04-22 23:19:52 +0200

    Troche wstąpnych konfiguracji i przygotowań.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <QString>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     cout << "MSPverifier" << endl;
+
+    //<TODO> inicjalizacja i czytanie argumentów
+
+    try
+    {
+        //<TODO>
+    }
+    catch(QString s)
+    {
+        // <TODO> put error
+    }
 
     return 0;
 }
