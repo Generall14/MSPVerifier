2611845b8b484cdfa51c92a57f8720461f771c09
Wojciech Kogut <wojciech.kogut@polon-alfa.pl>
2018-08-27 10:07:02 +0200

    Trochę opisów w mainpage.dox.

--- src/MSPVerifier.hpp
+++ src/MSPVerifier.hpp
@@ -6,8 +6,11 @@
 namespace MSPV
 {
 
+    /**
+     * Funkcja wszystkie wymagane czynności w symulacji, tj. pobieranie listy plików obiektem klasy FileList, wyciąganie z niej listy funkcji obiektem FunContainer, pobieranie spisu konwencji Convs oraz wykonywanie symulacji na obiekcie listy funkcji.
+     * @param ewpFile - adres pliku projektu IAR *.ewp.
+     * @param convsFile - adres pliku opisu konwencji wywołań.
+     */
     void verify(QString ewpFile, QString convsFile);
 
 }
-
-#endif
